9675ef3fb6d85d60bc4b6455c38d070b54ac3bd8
Azimul Haque <mailofazim@gmail.com>
2021-09-11 15:35:01 +0530

    first-commit

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -1,17 +1,17 @@
 // 1. Define a variable named `students` and assign a blank object to it.
-
+var students = {};
 // 2. Add a key of the value of variable `studentName` in the object created above with the value of your name.
-
+students.studentName = " Azimul";
 // 3. Using `console.log` log the value of `studentName` key from the `students` object
-
+console.log(students.studentName);
 // 4. Add a another key of the value of variable `batch` in the object with the value of 16.
-
+students.batch = 16;
 // 5. Using `console.log` log the value of `batch` key from the `students` object
-
+console.log(students.batch);
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
-
+students.isAdult = "true / false";
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
-
+console.log();
 // 8. Add a another key of `42` to the object with a value of `The answer to the meaning of life`.
 
 // 9. Using `console.log` print the value of key `42` from the `students` object
