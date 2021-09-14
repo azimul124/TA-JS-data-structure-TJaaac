2bdb10201420c106f1e875bb1080a49b9b390568
Azimul Haque <mailofazim@gmail.com>
2021-09-14 10:05:13 +0530

    first-commit

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -1,37 +1,63 @@
 let movies = [
   {
-    id: '1',
-    title: 'Game Night',
-    year: '2018',
-    genres: ['Action', 'Comedy', 'Crime'],
-    releaseDate: '2018-02-28',
-    actors: ['Rachel McAdams', 'Jesse Plemons', 'Jason Bateman'],
+    id: "1",
+    title: "Game Night",
+    year: "2018",
+    genres: ["Action", "Comedy", "Crime"],
+    releaseDate: "2018-02-28",
+    actors: ["Rachel McAdams", "Jesse Plemons", "Jason Bateman"],
   },
   {
-    id: '2',
-    title: 'Area X: Annihilation',
-    year: '2018',
-    genres: ['Adventure', 'Drama', 'Fantasy'],
-    releaseDate: '2018-02-23',
-    actors: ['Tessa Thompson', 'Jennifer Jason Leigh', 'Natalie Portman'],
+    id: "2",
+    title: "Area X: Annihilation",
+    year: "2018",
+    genres: ["Adventure", "Drama", "Fantasy"],
+    releaseDate: "2018-02-23",
+    actors: ["Tessa Thompson", "Jennifer Jason Leigh", "Natalie Portman"],
   },
   {
-    id: '3',
-    title: 'Hannah',
-    year: '2017',
-    genres: ['Drama'],
-    releaseDate: '2018-01-24',
-    actors: ['Charlotte Rampling', 'Andr Wilms', 'Phanie Van Vyve'],
+    id: "3",
+    title: "Hannah",
+    year: "2017",
+    genres: ["Drama"],
+    releaseDate: "2018-01-24",
+    actors: ["Charlotte Rampling", "Andr Wilms", "Phanie Van Vyve"],
+  },
+];
+let movies = [
+  {
+    id: "1",
+    title: "Game Night",
+    year: "2018",
+    genres: ["Action", "Comedy", "Crime"],
+    releaseDate: "2018-02-28",
+    actors: ["Rachel McAdams", "Jesse Plemons", "Jason Bateman"],
+  },
+  {
+    id: "2",
+    title: "Area X: Annihilation",
+    year: "2018",
+    genres: ["Adventure", "Drama", "Fantasy"],
+    releaseDate: "2018-02-23",
+    actors: ["Tessa Thompson", "Jennifer Jason Leigh", "Natalie Portman"],
+  },
+  {
+    id: "3",
+    title: "Hannah",
+    year: "2017",
+    genres: ["Drama"],
+    releaseDate: "2018-01-24",
+    actors: ["Charlotte Rampling", "Andr Wilms", "Phanie Van Vyve"],
   },
 ];
 
 /*
-Go through the variable named movies properly before solvinf the exercises.
+Go through the variable named movies properly before solving the exercises.
 Use the variable named movies to solve the requirement below.
 */
 
 // 1. Log the value of the title key in the first movie of the array named `movies`
-
+console.log(movies.id[1].title);
 // 2. Log the value of the title key in the last movie of the array named `movies`
 
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
