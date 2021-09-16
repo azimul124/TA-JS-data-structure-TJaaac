ce10ab906a0aa5cc3716ee9d27c570327031d304
Azimul Haque <mailofazim@gmail.com>
2021-09-16 12:19:42 +0530

    first-commit

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -57,3 +57,43 @@ Hint: (Structure of got variable)
 // 19. Log the value and length `allDescription`
 
 // 20. Only the the description whose length is more than 30. (use .length property on string to get the length)
+
+// 1. Log the value of key "houses"
+
+// 2. Log the length of the "houses" key
+
+// 3. Log the name of all houses in got one by one
+
+// 4. Log the name of all the people in house Starks (index 0) one by one
+
+// 5. Log the name of all the people in house Lannisters (index 1) one by one
+
+// 6. Log the description of all the people in house Lannisters (index 1) one by one
+
+// 7. Log the name and description of all the people in house Lannisters (index 1) one by one like `I am [name] and my bio is [description]`
+
+// 8. Log the name and description of all the people in house Starks (index 0) one by one like `I am [name] and my bio is [description]`
+
+// 9. Log the name and description of the people in house Starks whose name is `Robb Stark`
+
+// 10. Log the name and description of the people in house Lannisters whose name is `Tywin Lannister`
+
+// 11. Push the name of the houses in an array named `houseNames` and Log the array
+
+// 12. Log the size of people in Starks house
+
+// 13. Log the size of people in Lannisters house
+
+// 14. Add the name and size of people in an object like {Starks: 4, Lannisters: 6} and log the object
+
+// 15. Log the name of all the people of all the houses in got one by one
+
+// 16. Push all names into a new array named `allPeople` and log the value array.
+
+// 17. Log the value and length of `allPeople` array created above
+
+// 18. Push all descriptions into an array named `allDescription`
+
+// 19. Log the value and length `allDescription`
+
+// 20. Only the the description whose length is more than 30. (use .length property on string to get the length)
